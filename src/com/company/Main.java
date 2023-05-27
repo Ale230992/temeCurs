c50f28d1cdc21844653fc4fa85bc1d319e867b01
Ale230992 <alexandranicoara92@yahoo.ro>
2023-05-27 17:45:18 +0300

    teme curs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -60,6 +60,28 @@ public class Main<tablouFructe> {
         boolean b1 = Arrays.asList(array1).contains("d");
         System.out.println(b1);
 
+        //Problema 1 ppt 2(3) slide 53
+
+        int [] sir = {1, 5, 2, 8, 9, 14, 3, 4, 17, 10, 11};
+        int maxim = 0;
+        for(int t=0; t<sir.length; t=t+1){
+            if (sir[t]>maxim)
+                maxim=sir[t];
+            System.out.println("Maximul este"+maxim);
+        }
+
+        //Problema 2 ppt 2(3) slide 55
+
+        int nr=4;
+        if(nr>=5 && nr<=10){
+            System.out.println("Numarul"+ nr +"se afla in intervalul [5,10]");
+        }
+        else{
+            System.out.println("Numarul" + nr +" nu se afla in intervalul [5,10]");
+        }
+
+        //Problema 8 tema ppt 2(3) slide 60
+
         int n = 5;  //just taking n=5
         int sum = 0;
         int x=1;
